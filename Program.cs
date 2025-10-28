fc5854b777c9ad0ca7bcb31744426813f8ec2eb9
abdifatahWB <abfatz49@gmail.com>
2025-10-28 13:20:58 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uppgift_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int mynum = 10;
+            int mynum2 = 11;
+            string name = "abdi";
+            Console.WriteLine (name);
+            Console.WriteLine(mynum);
+            Console.WriteLine(mynum2);
+        }
+    }
+}
