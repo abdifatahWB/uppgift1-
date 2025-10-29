4288d71a379ff87ea8d10ef7163c1df0310299b6
abdifatahWB <abfatz49@gmail.com>
2025-10-29 13:35:46 +0200

    Add files via upload

---
+++ uppgift4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uppgift4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 1;
+
+            if (age >= 18)
+            {
+                Console.WriteLine("yes old enough to vote");
+            }
+            else 
+            {
+                Console.WriteLine("NO! your not old enough to vote, try next year again");
+            }
+                
+        }
+    }
+}
