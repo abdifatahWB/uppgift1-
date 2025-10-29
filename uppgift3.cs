7a94067b14586374b8ca40b07cacc6b307ae2ed6
abdifatahWB <abfatz49@gmail.com>
2025-10-29 12:26:30 +0200

    Add files via upload

---
+++ uppgift3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uppgift3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter username:");
+
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Username is: " + username );
+
+
+
+
+        }
+    }
+}
