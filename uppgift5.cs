d6bcd6c55d8f881374ac31ca378abd626d942479
abdifatahWB <abfatz49@gmail.com>
2025-10-30 12:46:43 +0200

    Add files via upload

---
+++ uppgift5.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uppgift5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = 4;
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("monday");
+                    break;
+                case 2:
+                    Console.WriteLine("tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("friday");
+                    break;
+                case 6:
+                    Console.WriteLine("saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("sunday");
+                    break;
+                }
+
+
+
+
+
+
+
+
+            }
+        }
+    }
+       
